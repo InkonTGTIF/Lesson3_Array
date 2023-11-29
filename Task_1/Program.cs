74fea13f32a0371d5583366963d1fc9b061f4d92
Karpov Kirill <maccdig@gmail.com>
2023-11-30 02:01:03 +0300

    Update tasks

---
+++ Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿int [] CreateArray(int minNum, int maxNum, int size){
+
+    int [] array = new int [size];
+    for(int i = 0; i<size; i++){
+        array[i] = new Random().Next(minNum, maxNum+1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array, int fromNum, int toNum){
+    int number = 0;
+    for(int i = 0; i<array.Length;i++){
+        if (array[i]>fromNum && array[i]<toNum)
+        number+=1;
+        Console.Write(array[i] + " ");
+        }
+    Console.WriteLine($"=> " + number);
+}
+
+int minNum = 1;
+int maxNum = 90;
+int size = 10;
+int fromNum = 20;
+int toNum = 90;
+
+int [] array = CreateArray(minNum, maxNum, size);
+
+ShowArray(array, fromNum, toNum);
---
+++ Task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿double [] CreateArray(int minNum, int maxNum, int size){
+
+    double [] array = new double [size];
+    Random random = new Random();
+    for(int i = 0; i<size; i++){
+        array[i] = Math.Round(random.NextDouble() * (maxNum - minNum) + minNum, 2);
+    }
+    return array;
+}
+
+void ShowArray(double [] array){
+    double minValue = array[0];
+    double maxValue = array[0];
+    double diff = 0;
+    for(int i = 0; i<array.Length;i++){
+        if (array[i]>maxValue){
+            maxValue = array[i];
+        }
+        else if (array[i]<minValue){
+            minValue = array[i];
+        }
+        Console.Write(array[i] + " ");
+    }
+    diff = maxValue-minValue;
+    Console.WriteLine($"=> " + diff);
+}
+
+int minNum = 1;
+int maxNum = 10;
+int size = 10;
+
+double [] array = CreateArray(minNum, maxNum, size);
+
+ShowArray(array);
